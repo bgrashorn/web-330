6349550d44ea615c1a384a785fa28be8c0c782d9
Brett Grashorn <brettgrashorn@gmail.com>
2023-01-05 22:45:16 -0600

    Comments Added

--- theme.js
+++ theme.js
@@ -1,3 +1,16 @@
+/**
+ =========================================================
+ ; Title: theme.js
+ ; Author: Brett Grashorn
+ ; Date: 5 January 2023
+ ; Description: JavaScript code for Assignment 1.
+ ; Sources:  Web 330 HTML, CSS, and JavaScript Requirements 
+ ;          document
+ ;          Assign_1 document
+  ========================================================
+ */
+
+//   Makes Light Theme the default if not already set
 function setDefaultTheme() {
     const theme = localStorage.getItem("mode") || "light-theme";
     const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
@@ -8,6 +21,7 @@ function setDefaultTheme() {
     document.getElementById("icon-text").innerHTML = iconText;
 }
 
+// Sets the body to user selected theme.
 function setSelectedTheme() {
     document.body.classList.value = localStorage.getItem("mode") || "light-theme";
 }
