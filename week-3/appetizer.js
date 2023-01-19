8a0ed0441e42da04a2585472b441e7bde21d952d
Brett Grashorn <brettgrashorn@gmail.com>
2023-01-18 19:03:36 -0600

    Week 3 Setup

---
+++ week-3/appetizer.js
@@ -0,0 +1,11 @@
+/**
+ =========================================================
+ ; Title: appetizer.js
+ ; Author: Brett Grashorn
+ ; Date: 17 January 2023
+ ; Description: JavaScript code for Assignment 3.
+ ; Sources:  Web 330 HTML, CSS, and JavaScript Requirements 
+ ;          document
+ ;          Assign_3 document
+  ========================================================
+ */
